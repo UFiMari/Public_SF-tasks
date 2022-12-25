2ca873eeb55ed5b561f308043bbd8ecd0e9b302c
Filatova Marina <uhlovskaya.filatovamn@yandex.ru>
2022-12-25 13:24:41 +0300

    Create base_page.py

---
+++ 28.1. Итоговый проект по автоматизации тестирования/base_page.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+    def __init__(self, driver, timeout=10):
+        self.driver = driver
+        self.base_url = "https://b2c.passport.rt.ru"
+        self.driver.implicitly_wait(timeout)
+
+
+    def go_to_site(self):
+        return self.driver.get(self.base_url)
+
+    def find_element(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator), message=f"Not found {locator}")
