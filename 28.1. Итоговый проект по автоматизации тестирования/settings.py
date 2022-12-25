21065d5be82160717c6c9149a7daab9016be2c21
Filatova Marina <uhlovskaya.filatovamn@yandex.ru>
2022-12-25 13:28:29 +0300

    Create settings.py

---
+++ 28.1. Итоговый проект по автоматизации тестирования/settings.py
@@ -0,0 +1,24 @@
+class Settings:
+    VALID_PHONE = "+79043331616"
+    VALID_PHONE_PASS = "K!@agdb@r4w3l6"
+    INVALID_PHONE = "+7043331616"
+    INVALID_PHONE_PASS = "K!@agdb@r4w3l"
+    ZERO_LOGIN = ""
+    ZERO_PASS = ""
+    VALID_MAIL = "testovayamarina.marina@yandex.ru"
+    VALID_MAIL_PASS = "j&.YemvCWfWv4MJ"
+    INVALID_MAIL = "testovayamarina@yandex.ru"
+    INVALID_MAIL_PASS = "j&.YemvCWfWv4"
+    NEW_VALID_PHONE_PASS = "K!@agdb@r4w3l6n"
+
+    INVALID_LOGIN = "testskillfactory"
+    INVALID_LOGIN_PASS = "K!@agDB@R4w3L"
+
+    INVALID_LS = "12345679"
+    INVALID_LS_PASS = "K!@agDB@R4w3L"
+
+    NEW_USER_NAME = "Марина"
+    NEW_UESR_SURNAME = "Маринина"
+    NEW_MAIL = "testovayamarina.marina@yandex.ru"
+    NEW_PHONE = "+79043331616"
+    NEW_REG_PASS = "Marina_1616"
